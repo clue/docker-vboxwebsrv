e14f0eb39ba2728f2c00ad010327622c6b378b67
Christian Lück <christian@lueck.tv>
2014-05-31 00:40:19 +0200

    Initial commit

---
+++ run.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+[ -z "$1" ] && echo "Error: No target argument given" && exit 1;
+
+# remote PORT can be set via ENV or defaults to 5678
+if [[ "${PORT:-0}" = "0" ]]; then
+  PORT=5678
+fi
+
+ssh -L 0.0.0.0:18083:localhost:$PORT $1 "killall vboxwebsrv; vboxwebsrv -p $PORT -A null"
+
