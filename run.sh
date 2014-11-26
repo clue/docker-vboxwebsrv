6d330130f33d51a038032a925991e41b56920abe
UserTaken <elysian@live.ca>
2014-11-26 13:04:26 -0800

    Update

--- run.sh
+++ run.sh
@@ -1,11 +1,8 @@
-#!/bin/bash
-
-[ -z "$1" ] && echo "Error: No target argument given" && exit 1;
+[ -z "$1" ] && echo -e "\x1B[01;31mError: No SSH address given.\x1B[0m" && exit 1;
 
 # remote PORT can be set via ENV or defaults to 5678
 if [[ "${PORT:-0}" = "0" ]]; then
   PORT=5678
 fi
 
-ssh -L 0.0.0.0:18083:localhost:$PORT $1 "killall vboxwebsrv; vboxwebsrv -p $PORT -A null"
-
+ssh -CL 0.0.0.0:18083:127.0.0.1:$PORT -L 0.0.0.0:3389:127.0.0.1:3389  -L 0.0.0.0:3390:127.0.0.1:3390 -L 0.0.0.0:3391:127.0.0.1:3391 $1 "killall vboxwebsrv && vboxwebsrv -p $PORT -A null -t 0"
