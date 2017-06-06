5c99a0d74ad2150188d893d2846a373a8a397589
vegardsaglien <vegard@saglien.com>
2017-06-06 11:49:22 +0200

    Disabled StrictHostKeyChecking

--- run.sh
+++ run.sh
@@ -7,5 +7,5 @@ if [[ "${PORT:-0}" = "0" ]]; then
   PORT=5678
 fi
 
-ssh -L 0.0.0.0:18083:localhost:$PORT $1 "killall vboxwebsrv; vboxwebsrv -p $PORT -A null"
+ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no -L 0.0.0.0:18083:localhost:$PORT $1 "killall vboxwebsrv; vboxwebsrv -p $PORT -A null"
 
